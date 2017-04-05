5640c842d809338e57f8144fd540dd2d12d9930e
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-04-05 15:51:25 -0400

    add a test for new keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -19,6 +19,11 @@ let result = add(five, ten);
 `,
 		`!-/*67`,
 		`3 < x > 9`,
+		`if (5 < 10) {
+  return true;
+} else {
+  return false;
+}`,
 	}
 
 	results := [][]struct {
@@ -88,6 +93,25 @@ let result = add(five, ten);
 			{token.IDENT, "x"},
 			{token.GT, ">"},
 			{token.INT, "9"},
+		},
+		{
+			{token.IF, "if"},
+			{token.LPAREN, "("},
+			{token.INT, "5"},
+			{token.LT, "<"},
+			{token.INT, "10"},
+			{token.RPAREN, ")"},
+			{token.LBRACE, "{"},
+			{token.RETURN, "return"},
+			{token.TRUE, "true"},
+			{token.SEMICOLON, ";"},
+			{token.RBRACE, "}"},
+			{token.ELSE, "else"},
+			{token.LBRACE, "{"},
+			{token.RETURN, "return"},
+			{token.FALSE, "false"},
+			{token.SEMICOLON, ";"},
+			{token.RBRACE, "}"},
 		},
 	}
 
